6951be6ce3318e253460a5f2244b8bcd1f846ad7
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-12-20 15:47:13 -0800

    enemies will now avoid switching targets if their current one has a higher aggro stat

--- HDeItems/AggroManager.cs
+++ HDeItems/AggroManager.cs
@@ -1,11 +1,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using RoR2;
+using RoR2.CharacterAI;
 
 namespace HDeMods.HDeItems {
     public static class AggroManager {
         internal static void Init() {
             On.RoR2.BullseyeSearch.GetResults += BullseyeSearch_GetResults;
+            On.RoR2.CharacterAI.BaseAI.OnBodyDamaged += BaseAI_OnBodyDamaged;
+        }
+
+        private static void BaseAI_OnBodyDamaged(On.RoR2.CharacterAI.BaseAI.orig_OnBodyDamaged orig, 
+            BaseAI self, DamageReport damagereport) {
+            BodyData enemyBodyData = damagereport.damageInfo.attacker.GetComponent<BodyData>();
+            BodyData currentTargetData = self.currentEnemy.gameObject.GetComponent<BodyData>();
+            if (!enemyBodyData || !currentTargetData) {
+                orig(self, damagereport);
+                return;
+            }
+            if (enemyBodyData.Aggro >= currentTargetData.Aggro) orig(self, damagereport);
         }
 
         // Thanks to KingEnderBrine, Chinchi, Violetchaolan, and Harb for this... creature...
