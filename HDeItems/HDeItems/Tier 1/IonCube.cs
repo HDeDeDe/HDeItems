6d9b5bb55e15f24a596a1771fabe5c2f7104af7d
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-12-26 16:12:02 -0800

    Add ion cube

---
+++ HDeItems/HDeItems/Tier 1/IonCube.cs
@@ -0,0 +1,32 @@
+using RoR2;
+using R2API;
+using BepInEx.Configuration;
+
+namespace HDeMods.HDeItems.Tier1 {
+    [HDeItem] public class IonCube {
+        public static ConfigEntry<bool> Enabled { get; set; }
+        public static ItemDef item;
+        
+        public static void HDeItem_Init() {
+            Enabled = Plugin.instance.Config.Bind<bool>(
+                "Items - Tier 1",
+                "IonCube",
+                true,
+                "Enables Ion Cube."
+            );
+            if (Options.RoO.Enabled) Options.RoO.AddCheck(Enabled, true);
+
+            if (!Enabled.Value) return;
+            item = ItemManager.HDeItemsBundle.LoadAsset<ItemDef>("HDe_IonCubeDef");
+            if (item == null) {
+                Log.Error("Failed to load " + nameof(IonCube));
+                return;
+            }
+
+            CustomItem customItem = new CustomItem(item, new[] { new ItemDisplayRule() });
+            ItemAPI.Add(customItem);
+            
+            Log.Info("Successfully loaded " + nameof(IonCube));
+        }
+    }
+}
